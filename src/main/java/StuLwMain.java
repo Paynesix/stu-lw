7b848afd26f67a6db4f1f560ce72c312577035c6
wu_xy <wu_xy@gzhc365.com>
2020-03-17 10:48:11 +0800

    start study

---
+++ src/main/java/StuLwMain.java
@@ -0,0 +1,12 @@
+/**
+ * @Description TODO
+ * @Author xy
+ * @Date 2020/3/17 10:45
+ * @Version 1.0
+ * @Since JDK 1.8
+ */
+public class StuLwMain {
+    public static void main(String[] args) {
+        System.out.println("Stu Lw Main");
+    }
+}
---
+++ src/test/java/stu/lw/service/DubboProviderStart.java
@@ -0,0 +1,7 @@
+package stu.lw.service;
+
+public class DubboProviderStart {
+    public static void main(String[] args) {
+        System.out.println("Hello lw");
+    }
+}
